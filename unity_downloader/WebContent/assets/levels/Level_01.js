fc13f450472d56b177676afbe20e2c462c7933b4
Dawid <dawid.kubic@gmail.com>
2018-01-27 19:25:06 +0100

    updated wifi

--- unity_downloader/WebContent/assets/levels/Level_01.js
+++ unity_downloader/WebContent/assets/levels/Level_01.js
@@ -258,7 +258,7 @@ Level_01.prototype.addRouter = function () {
 		return false;
 	}
 	cash -= 20;
-	var _wifi = this.add.sprite(210, 100, "wifi");
+	var _wifi = this.add.sprite(250, 5, "wifi");
 	_wifi.animations.add('beep', [0,1,2,3,2,1,0], 3, true);
 	_wifi.animations.play('beep');
 	_wifi.inputEnabled = true;

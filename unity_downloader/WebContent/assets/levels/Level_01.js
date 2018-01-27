44209825cac3f30ddf852a125a3548170b45798f
Bart <pankowski.bartosz@gmail.com>
2018-01-27 20:06:32 +0100

    wifi idle animation

--- unity_downloader/WebContent/assets/levels/Level_01.js
+++ unity_downloader/WebContent/assets/levels/Level_01.js
@@ -259,7 +259,7 @@ Level_01.prototype.addRouter = function () {
 	}
 	cash -= 20;
 	var _wifi = this.add.sprite(210, 100, "wifi");
-	_wifi.animations.add('beep', [0,1,2,3,2,1,0], 3, true);
+	_wifi.animations.add('beep', [0,1,2,3,4], 3, true);
 	_wifi.animations.play('beep');
 	_wifi.inputEnabled = true;
 	_wifi.input.enableDrag();
